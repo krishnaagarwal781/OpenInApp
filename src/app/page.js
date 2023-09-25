736ea1d5a7525136afa9986b8e730495fd2ccfd9
Abhishek khare <ghostabhi323@gmail.com>
2023-09-25 18:21:29 +0530

    fixed the responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -26,30 +26,30 @@ export default function Home() {
       </div>
 
       <div
-        className="lg:hidden w-full h-full relative left-0 top-0 bg-gradient-to-b from-[#4285F4] to-[#286DE0]"
+        className="lg:hidden overflow-hidden w-full h-full relative left-0 top-0 bg-gradient-to-b from-[#4285F4] to-[#286DE0]"
         // style={{ clipPath: "polygon(0 0, 100% 0%, 80% 100%, 0% 100%)" }}
       >
-        <div className="absolute w-69 h-29 left-1/2 top-14 font-poppins font-bold text-24 leading-[122.69%] tracking-[0.015em] text-white">
+        <div className="absolute w-full h-29 left-[46%] top-14 font-poppins font-bold text-24 leading-[122.69%] tracking-[0.015em] text-white">
           Logo
         </div>
 
-        <div className="absolute w-full left-[46%] top-24 font-montserrat font-bold text-4xl leading-9 text-white">
+        <div className="absolute w-full left-[38%] top-24 font-montserrat font-bold text-4xl leading-9 text-white">
           Board.
         </div>
-        <div className="flex absolute h-12 bottom-10 left-36 justify-between w-1/4">
-          <BsGithub size="30px" color="white" />
-          <AiFillTwitterCircle size="30px" color="white" />
-          <AiFillLinkedin size="30px" color="white" />
-          <BiLogoDiscord size="30px" color="white" />
+        <div className="flex absolute h-12 bottom-10 left-[35%] gap-4 justify-between w-32">
+          <BsGithub size="35px" color="white" />
+          <AiFillTwitterCircle size="35px" color="white" />
+          <AiFillLinkedin size="35px" color="white" />
+          <BiLogoDiscord size="35px" color="white" />
         </div>
       </div>
 
-      <div className="sm:hidden md:hidden lg:block w-[50vw] h-[100vh] bg-[#F8FAFF]">
+      <div className="hidden lg:block w-[50vw] h-[100vh] bg-[#F8FAFF]">
         <div className="absolute right-56 top-44">
           <SignInBox/>
         </div>
       </div>
-      <div className="hidden sm:block md:block lg:hidden w-50vw min-h-screen bg-[#F8FAFF]">
+      <div className="lg:hidden w-50vw min-h-screen bg-[#F8FAFF]">
         <div className="absolute top-44 left-1/2 transform -translate-x-1/2">
           <SignInBox />
         </div>
--- src/components/SignInBox.jsx
+++ src/components/SignInBox.jsx
@@ -5,16 +5,18 @@ import { AiFillApple } from 'react-icons/ai'
 
 const SignIn = () => {
   return (
-    <div className='w-auto h-full -mt-4'>
-      <div className='font-montserrat font-bold text-4xl leading-[44px] text-black'>
+    <div className='w-auto h-full -mt-4 '>
+      <div className='font-montserrat font-bold text-4xl leading-[44px] text-white lg:text-black'>
         Sign In
       </div>
-      <div className="font-Lato font-normal text-base leading-[19px] mt-2 text-black">Sign in to your account</div>
-      <div className='flex mt-8 items-center justify-between gap-2'>
-        <div className='flex px-5 py-2 bg-white font-normal text-xs text-[#858585] font-montserrat rounded-xl items-center justify-around'><FcGoogle className='mr-3' size={20} /> <span className='mr-2'> Sign in with Google</span></div>
-        <div className='flex px-4 py-2 bg-white font-normal text-xs text-[#858585] font-montserrat rounded-xl items-center justify-around'><AiFillApple className='mr-3' size={20}/> <span className='mr-2'>Sign in with Apple</span> </div>
+      <div className="font-Lato font-normal text-base leading-[19px] mt-2 text-white lg:text-black">Sign in to your account</div>
+
+      <div className='flex flex-col lg:flex-row mt-8 items-center justify-between gap-2'>
+        <div className='flex w-full lg:w-auto px-5 py-2 bg-white font-normal text-xs text-[#858585] font-montserrat rounded-xl items-center justify-around'><FcGoogle className='mr-3' size={20} /> <span className='mr-2'> Sign in with Google</span></div>
+        <div className='flex w-full lg:w-auto px-4 py-2 bg-white font-normal text-xs text-[#858585] font-montserrat rounded-xl items-center justify-around'><AiFillApple className='mr-3' size={20}/> <span className='mr-2'>Sign in with Apple</span> </div>
       </div>
-      <form className='bg-white rounded-xl mt-8 p-4 relative'>
+
+      <form className='bg-white w-[80vw] lg:w-full rounded-xl mt-4 lg:mt-8 pb-10 lg:pb-0 p-4 relative'>
         <div className="mb-6">
           <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Email Address</label>
           <input type="email" id="email" className="bg-[#F5F5F5] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
@@ -28,8 +30,8 @@ const SignIn = () => {
         </div>
         <button type="submit" className="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Submit</button>
       </form>
-      <div className='absolute -bottom-6 left-10'>
-          <div>Don&apos;t have an account? <span className='text-[#346BD4] cursor-pointer'>Register here</span></div>
+      <div className='absolute bottom-2 lg:-bottom-8 left-8 lg:left-10'>
+          <div className='text-sm lg:text-base'>Don&apos;t have an account? <span className='text-[#346BD4] cursor-pointer'>Register here</span></div>
       </div>
     </div>
   )
